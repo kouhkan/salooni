dc284c403fbb4efdf95dbca9df2ce676cc6a0d5d
amir kouhkan <amirkouhkan1@gmail.com>
2020-09-05 02:50:03 +0430

    remove docker files

--- scripts/db.sh
+++
@@ -1,5 +0,0 @@
-cat ./scripts/db.sh
-#!/bin/env bash
-psql -U postgres -c "CREATE USER $DB_USER PASSWORD '$DB_PASS'"
-psql -U postgres -c "CREATE DATABASE $DB_NAME OWNER $DB_USER"
-chmod a+rx ./scripts/db.sh
--- scripts/runserver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /code
-su  -c "python manage.py collectstatic --no-input && python manage.py makemigrations && python manage.py migrate && gunicorn --workers=3 myblog.wsgi -b 0.0.0.0:8080"
